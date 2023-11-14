8ecb62d9bb48a98ff60fef816c8aab6fa3945d76
AnaLuizaBAB <115850710+AnaLuizaBAB@users.noreply.github.com>
2023-11-14 14:59:54 -0300

    4. A sequência do jogo
    - Usando "while ( )" e "break"

--- Python começando com a linguagem.py
+++ Python começando com a linguagem.py
@@ -4,13 +4,22 @@ print ("Bem vindo ao jogo da adivinhação.")
 print("\n\n")
 
 numero_secreto = 43
-chute = int(input ("Informe o número inteiro positivo: "))
+numero_de_tentativas = 1
 
-print(f"\n\nVocê digitou o número {chute}.")
+while(numero_de_tentativas < 4):
 
-if (chute == numero_secreto):
-    print("Parabéns, resposta Certa! \n")
-elif (chute < numero_secreto):
-    print("Resposta errada.\nO número a ser adivinhado é maior! \n")
-else:
-    print ("Resposta errada.\nO número a ser adivinhado é menor!\n")
+    print(f"***Tentativa número {numero_de_tentativas} de 3***")
+    chute = int(input ("Por favor informe o número inteiro positivo: "))
+    print(f"\nVocê digitou o número {chute}. \n")
+    
+    if (chute == numero_secreto):
+        print("Parabéns, resposta Certa!\n")
+        break
+    elif (chute < numero_secreto):
+        print("Resposta errada.\nO número a ser adivinhado é maior\n!")
+    else:
+        print ("Resposta errada.\nO número a ser adivinhado é menor!\n")
+    
+    numero_de_tentativas += 1
+
+print("Fim de Jogo.\n")
