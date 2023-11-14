31f2625cde9140e4269891064243499c46c4587f
AnaLuizaBAB <115850710+AnaLuizaBAB@users.noreply.github.com>
2023-11-14 15:04:40 -0300

    5. Gerando números aleatórios.
    - Aprendendo sobre o módulo random.

--- Python começando com a linguagem.py
+++ Python começando com a linguagem.py
@@ -1,25 +1,30 @@
 
+import random
+
 print("\n\n")
 print ("Bem vindo ao jogo da adivinhação.")
-print("\n\n")
+print("\n")
 
-numero_secreto = 43
+numero_secreto = random.randrange(1,301)
 numero_de_tentativas = 1
 
 while(numero_de_tentativas < 4):
 
-    print(f"***Tentativa número {numero_de_tentativas} de 3***")
-    chute = int(input ("Por favor informe o número inteiro positivo: "))
-    print(f"\nVocê digitou o número {chute}. \n")
-    
-    if (chute == numero_secreto):
-        print("Parabéns, resposta Certa!\n")
+    print(f"\n*****Tentativa número {numero_de_tentativas} de 3*****")
+    chute = int(input ("Por favor informe o número de 1 a 300: "))
+    print(f"\nVocê digitou o número {chute}.")
+
+    if ( chute < 1 or chute >300):
+        print ("Número inválido. Por favor informe um número de 1 a 300.\n")
+        continue
+    elif (chute == numero_secreto):
+        print("Parabéns, resposta Certa! \n")
         break
     elif (chute < numero_secreto):
-        print("Resposta errada.\nO número a ser adivinhado é maior\n!")
+        print("Resposta errada.\nO número a ser adivinhado é maior!\n")
     else:
         print ("Resposta errada.\nO número a ser adivinhado é menor!\n")
     
     numero_de_tentativas += 1
 
-print("Fim de Jogo.\n")
+print("\nFim de Jogo.")
